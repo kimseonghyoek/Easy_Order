e6abed8b7f621c1bc4beaea1c8e141edfb64dc00
Kimseonghyoek <cilo1223@naver.com>
2022-05-30 00:11:02 +0900

    Update App component and Add app style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import './app.scss'
+import Input from "./components/Input";
+
 
 const App = () => {
   return (
     <div>
-      <h1>환경 세팅</h1>
+      <h1 className="title">이지 오더</h1>
+      <h1>Easy Order</h1>
     </div>
   )
 }
