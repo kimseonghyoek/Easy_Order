0c0d2f6183a85492766f49d64b5346f5ca15201d
Kimseonghyoek <cilo1223@naver.com>
2022-05-31 12:04:02 +0900

    Change Webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,14 @@ module.exports = {
           'css-loader',
           'sass-loader'
         ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
       }
     ]
   },
