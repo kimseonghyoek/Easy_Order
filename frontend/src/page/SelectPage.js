2223d9d4f1fc5e109c44ff68cbf35ee307caf4b7
Kimseonghyoek <cilo1223@naver.com>
2022-06-02 03:19:08 +0900

    Done :)

---
+++ frontend/src/page/SelectPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../app.scss';
+
+const SelectPage = () => {
+  return (
+    <div id='titles'>
+      <h1 className="title">이지 오더</h1>
+      <h4 className="subTitle">음식 주문을 보다 쉽게</h4>
+      <h1>Easy Order</h1>
+    </div>
+  );
+}
+
+export default SelectPage;
