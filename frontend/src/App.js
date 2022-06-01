665d5fd6a82695dd10c212f065156bcb29fc820c
Kimseonghyoek <cilo1223@naver.com>
2022-06-02 03:16:31 +0900

    Add Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,16 @@ import Box from "./container/Box";
 import ceo from '../src/public/img/mapIMG.png';
 import { Link, Route, Routes } from "react-router-dom";
 import Login from "./page/Login";
+import SelectPage from "./page/selectPage";
 
 const App = () => {
   
   return (
     <div id="firstPage">
-      <h1 className="title">이지 오더</h1>
-      <h4 className="subTitle">음식 주문을 보다 쉽게</h4>
-      <h1>Easy Order</h1>
+      <Routes>
+        <Route path="/" element={<SelectPage/>}></Route>
+        <Route path="/ceoLogin" element={<Login/>}/>
+      </Routes>  
       <div className="wrap__Row">
         <Link to="/ceoLogin">
           <Box boxTitle="사장님"/>
@@ -20,9 +22,6 @@ const App = () => {
           <Box boxTitle="손님"/>
         </Link>
       </div>
-      <Routes>
-        <Route path="/ceoLogin" element={<Login/>}/>
-      </Routes>
     </div>
   )
 }
