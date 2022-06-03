45c4391ae6f49b1093e33ea4de8b2b37146a1245
Kimseonghyoek <cilo1223@naver.com>
2022-06-04 02:27:44 +0900

    Create Socal Component and style file

---
+++ frontend/src/components/Socal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './scss/socal.scss';
+
+const Socal = (props) => {
+  return (
+    <div id="wrapIcon">
+      <p id="pid">logo</p>
+    </div>
+  )
+}
+
+export default Socal;
