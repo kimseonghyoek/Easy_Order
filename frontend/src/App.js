f14907a9c89b788a588700110d7e5c32a8e72986
Kimseonghyoek <cilo1223@naver.com>
2022-06-04 02:28:51 +0900

    Chage App page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,16 @@
 import React from "react";
 import './app.scss'
-import Box from "./container/Box";
-import ceo from '../src/public/img/mapIMG.png';
 import { Link, Route, Routes } from "react-router-dom";
 import Login from "./page/Login";
 import SelectPage from "./page/selectPage";
 
 const App = () => {
-  
   return (
     <div id="firstPage">
       <Routes>
         <Route path="/" element={<SelectPage/>}></Route>
         <Route path="/ceoLogin" element={<Login/>}/>
       </Routes>  
-      <div className="wrap__Row">
-        <Link to="/ceoLogin">
-          <Box boxTitle="사장님"/>
-        </Link>
-        <Link to="/guestLogin">
-          <Box boxTitle="손님"/>
-        </Link>
-      </div>
     </div>
   )
 }
