8e8a18eff182a886e7e9e4fd98c80cc7505351a3
Kimseonghyoek <cilo1223@naver.com>
2022-06-07 00:45:07 +0900

    Update Login page

--- frontend/src/page/Login.js
+++ frontend/src/page/Login.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import Input from "../components/Input";
+import { Link } from "react-router-dom";
+import Socal from "../components/Socal";
 
 const Login = () => {
+
+  const [idState, setIdState] = React.useState('');
+  const [pwState, setPwState] = React.useState('');
+
   return (
-    <div>
-      <h1>로그인</h1>
+    <div id="loginWrap">
+      <h1 className="loginTitle">이지 오더</h1>
+      <div>
+        <Input placeholder="아이디"></Input>
+        <Input placeholder="비밀번호"></Input>
+        <button className="loginBtn">로그인</button>
+      </div>
+      <Link to="Signup">
+        <p id="signUp">아직 가입하지 않으셨나요?</p>
+      </Link>
+      <p>소셜 로그인</p>
+      <div className="wrap__Row">
+        <Socal logo="KAKAO"></Socal>
+        <Socal logo="Naver"></Socal>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
